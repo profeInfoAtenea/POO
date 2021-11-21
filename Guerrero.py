58c6b0626c63b5c9d61a0811a770130980037a6a
profeInfoAtenea <90715116+profeInfoAtenea@users.noreply.github.com>
2021-11-21 19:28:56 +0000

    Update Guerrero.py

--- Guerrero.py
+++ Guerrero.py
@@ -2,40 +2,60 @@ import random
 
 class Guerrero:
     def __init__(self,nombre, vida, fuerza, precision, velocidad, defensa):
-        self.nombre = nombre
-        self.vida = vida
-        self.fuerza = fuerza
-        self.precision = precision
-        self.velocidad = velocidad
-        self.defensa = defensa
-        self.image = '⚔'
+        self.__nombre = nombre
+        self.__vida = vida
+        self.__fuerza = fuerza
+        self.__precision = precision
+        self.__velocidad = velocidad
+        self.__defensa = defensa
+        self.__image = '⚔'
+    
+    def getVelocidad(self):
+        return self.__velocidad
+    
+    def getPrecision(self):
+        return self.__precision
+    
+    def getVida(self):
+        return self.__vida
+    def setVida(self, vida):
+        self.__vida = vida
+    def getFuerza(self):
+        return self.__fuerza
+    def getDefensa(self):
+        return self.__defensa
+    def getNombre(self):
+        return self.__nombre
+    
     def golpear(self, g):
-        if( random.random() <= self.precision - g.velocidad / 100):
-            g.vida -= max([(self.fuerza - g.defensa)+random.randrange(-10,11),1])
-            print("Golpe certero de ", self.nombre)
+        #veo si acierto el golpe
+        if( random.random() <= self.getPrecision() - g.getVelocidad() / 100):
+            #en caso de acertar, agrego daño al oponente
+            g.setVida(g.getVida() - max([(self.getFuerza() - g.getDefensa())+random.randrange(-10,11),1]))
+            print("Golpe certero de ", self.getNombre())
         else:
-            print(g.nombre , " esquiva el golpe")
-            
-def simularbatalla(j1, j2):
-    #comienza jugador  más veloz
+            print(g.nombre , " esquiva el golpe")   
+
+def simular_batalla(j1, j2):
+    #comienza jugador más veloz
     golpeador, receptor = j1, j2
-    if(j1.velocidad < j2.velocidad):
+    if(j1.getVelocidad() < j2.getVelocidad()):
         golpeador, receptor = j2, j1
-        
-    #Se golpean hasta que alguno tenga vida cero
-    while(j1.vida > 0 and j2.vida>0):
-        print("\n" + j1.nombre, j1.vida, "vs", j2.nombre, j2.vida)
+    
+    while(j1.getVida() > 0 and j2.getVida() > 0):
         golpeador.golpear(receptor)
         #cambio de turnos
-        golpeador, receptor = receptor, golpeador
-    #fin
-    print("\n" + j1.nombre, j1.vida , "vs" , j2.vida, j2.nombre)
-    print("Ganador:", receptor.nombre)
+        golpeador , receptor = receptor , golpeador
+        #fin
+    print("\n" + j1.getNombre() , j1.getVida() , "vs", j2.getVida(), j2.getNombre())
+    print("Ganador: ", receptor.getNombre())
 
-# batalla de ejemplo
-superman = Guerrero("Superman", 100, 50, 80, 30, 20)
-goku = Guerrero("Goku", 100, 60, 80, 40, 20)
-chuck = Guerrero("Chuck norris", 200, 99, 99,99, 99)
+#batalla de ejemplo
+    #     Guerrero(nombre:'Superman', vida:100, fuerza:50, precision:80, velocidad:30, defensa:20)
+superman = Guerrero('Superman', 100, 50, 60, 30, 20)
+goku = Guerrero('Goku', 100, 75, 50, 95, 85)
+chuck = Guerrero('Chuck-Norris', 100, 10, 40, 15, 5)
+brucelee = Guerrero('BruceLee', 100, 15, 50, 16, 6)
 
-#Simula batalla
-simularbatalla(goku,superman)
+#simulamos batalla
+simular_batalla(chuck, brucelee)
