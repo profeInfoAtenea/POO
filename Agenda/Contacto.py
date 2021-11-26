ddbd99d7e918e92ed9fa3d414c42c73bb8003e11
profeInfoAtenea <90715116+profeInfoAtenea@users.noreply.github.com>
2021-11-26 10:18:31 +0000

    Update Contacto.py

--- Agenda/Contacto.py
+++ Agenda/Contacto.py
@@ -7,27 +7,27 @@ class Contacto:
     def __str__(self):
         return "Nombre:  {0} Telefono: {1} Email : {2}".format(self.__nombre, self.__telefono, self.__email)
     
-    def getTelefono(self):
+    def get_telefono(self):
         """Get Telefono"""
         return self.__telefono
-    def setTelefono(self, telefono):
+    def set_telefono(self, telefono):
         self.__telefono = telefono
 
-    def getNombre(self):
+    def get_nombre(self):
         return self.__nombre
-    def setNombre(self, nombre):
+    def set_nombre(self, nombre):
         self.__nombre = nombre
 
-    def getEmail(self):
+    def get_email(self):
         return self.__email
     
-    def setEmail(self, email):
+    def set_email(self, email):
         self.__email = email
         
 '''
 c1 = Contacto("manuel", "666.666.666", "c1@email.test")
 
 c1.nombre = "Lucia"
-c1.setTelefono("777")
+c1.set_telefono("777")
 print(str(c1))
 '''
