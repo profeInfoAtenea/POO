36f5290f775da3897d819bf1ace926e722042291
profeInfoAtenea <90715116+profeInfoAtenea@users.noreply.github.com>
2021-11-26 12:20:07 +0000

    Update __main__.py

--- Agenda/__main__.py
+++ Agenda/__main__.py
@@ -1,19 +1,17 @@
 from Contacto import Contacto
 from Agenda import Agenda
 
-
-
-def crearAgendaPrueba():
+def crear_agenda_para_prueba():
+    """Crea una Agenda con algunos datos para probar"""
     c1 = Contacto("Manuel", "666.111.666", "c1@email.test")
     c2 = Contacto("Lucia", "666.222.666", "c2@email.test")
     c3 = Contacto("Marta", "666.333.666", "c2@email.test")
     a = Agenda()
-    a.agregarContacto(c1)
-    a.agregarContacto(c2)
-    a.agregarContacto(c3)
+    a.agregar_contacto(c1)
+    a.agregar_contacto(c2)
+    a.agregar_contacto(c3)
     return a
 
 if __name__ == '__main__':
-    a = crearAgendaPrueba()
-    a.abrirAgenda()
- 
+    a = crear_agenda_para_prueba()
+    a.abrir_agenda()
